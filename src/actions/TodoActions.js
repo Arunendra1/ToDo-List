9c5b1b4bd577840c4acd4ca8239e97d707f2a791
Arunendra Bhardwaj <mutursingh1111@gmail.com>
2024-06-29 20:15:05 +0530

    Update TodoActions.js

--- src/actions/TodoActions.js
+++ src/actions/TodoActions.js
@@ -1,27 +1,49 @@
-export const AddTodoAction=(todo)=>(dispatch,getState)=>{
- const {
-    Todo: {todos},
-  }=getState();
-  const hasTodo=todos.find((i)=>i.todo===todo);
-  if(!hasTodo && todo!==''){
-    dispatch({
-        type:"ADD_TODO",
-        payload:[{id:todo,todo},...todos],
-    });
-  }
+export const ADD_TODO = 'ADD_TODO';
+export const REMOVE_TODO = 'REMOVE_TODO';
+export const EDIT_TODO = 'EDIT_TODO';
+
+
+// Action to add a todo
+export const AddTodoAction = (todo) => (dispatch, getState) => {
+  const {
+    Todo: { todos },
+  } = getState();
+
+  const newTodos = [...todos, todo];
+  localStorage.setItem('todos', JSON.stringify(newTodos));// Save to local storage
+
+  dispatch({
+    type: ADD_TODO,
+    payload: todo,
+  });
 };
-export const RemoveTodoAction=(todo)=>(dispatch,getState)=>{
-    const {
-        Todo: {todos},
-    }=getState();
-    dispatch({
-        type:"REMOVE_TODO",
-        payload:todos.filter((t)=>t.id!==todo.id),
-    });
+// Action to remove a todo
+export const RemoveTodoAction = (todo) => (dispatch, getState) => {
+  const {
+    Todo: { todos },
+  } = getState();
+
+  const newTodos = todos.filter((t) => t.id !== todo.id);
+  localStorage.setItem('todos', JSON.stringify(newTodos));// Update local storage
+
+  dispatch({
+    type: REMOVE_TODO,
+    payload: todo,
+  });
 };
-export const EditTodoAction = (id, newTodo) => {
-  return {
-    type: 'EDIT_TODO',
-    payload: { id, newTodo }
-  };
+// Action to edit a todo
+export const EditTodoAction = (id, newTodo) => (dispatch, getState) => {
+  const {
+    Todo: { todos },
+  } = getState();
+
+  const newTodos = todos.map((todo) =>
+    todo.id === id ? { ...todo, todo: newTodo } : todo
+  );
+  localStorage.setItem('todos', JSON.stringify(newTodos));// Update local storage
+
+  dispatch({
+    type: EDIT_TODO,
+    payload: { id, newTodo },
+  });
 };
