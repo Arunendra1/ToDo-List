ad7c7bb10ca0acb69a76e1b315fb43765401bf7e
Arunendra Bhardwaj <mutursingh1111@gmail.com>
2024-06-29 20:17:10 +0530

    Update TodoReducer.js

--- src/reducer/TodoReducer.js
+++ src/reducer/TodoReducer.js
@@ -1,21 +1,39 @@
-const TodoReducer=(state={todos:[]},action)=>{
-    switch(action.type){
-        case 'ADD_TODO':
-              return {todos:action.payload};
+import { ADD_TODO, REMOVE_TODO, EDIT_TODO } from '../actions/TodoActions';
 
-        case 'REMOVE_TODO':
-        return {todos:action.payload};
+const initialState = {
+  todos: JSON.parse(localStorage.getItem('todos')) || [], // Initialize state from local storage
+};
 
-        case 'EDIT_TODO':
-            return {
-              ...state,
-              todos: state.todos.map(todo =>
-                todo.id === action.payload.id ? { ...todo, todo: action.payload.newTodo } : todo
-              )
-            };
+const TodoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      const updatedAddTodos = [...state.todos, action.payload];
+      localStorage.setItem('todos', JSON.stringify(updatedAddTodos));
+      return {
+        ...state,
+        todos: updatedAddTodos,
+      };
+    case REMOVE_TODO:
+      const updatedRemoveTodos = state.todos.filter(
+        (todo) => todo.id !== action.payload.id
+      );
+      localStorage.setItem('todos', JSON.stringify(updatedRemoveTodos));
+      return {
+        ...state,
+        todos: updatedRemoveTodos,
+      };
+    case EDIT_TODO:
+      const updatedEditTodos = state.todos.map((todo) =>
+        todo.id === action.payload.id ? { ...todo, todo: action.payload.newTodo } : todo
+      );
+      localStorage.setItem('todos', JSON.stringify(updatedEditTodos));
+      return {
+        ...state,
+        todos: updatedEditTodos,
+      };
+    default:
+      return state;
+  }
+};
 
-        default:
-            return state;
-    }
-}
 export default TodoReducer;
